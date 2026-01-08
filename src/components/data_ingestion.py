c183cd68aba67705568d4df2d800ef8abc1ae1e8
Samruddhiwagh-tech <samruddhiwagh445@gmail.com>
2026-01-08 12:30:08 +0530

    model trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,11 +1,13 @@
 import os
 import sys
-from src.exception import CustomException
-from src.logger import logging
+from dataclasses import dataclass
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from dataclasses import dataclass
+
+from src.exception import CustomException
+from src.logger import logging
 from src.components.data_transformation import DataTransformation
+from src.components.model_trainer import ModelTrainer
 
 
 @dataclass
@@ -22,39 +24,18 @@ class DataIngestion:
     def initiate_data_ingestion(self):
         logging.info("Entered the data ingestion method")
         try:
-            # ✅ correct path (change ONLY if your CSV location is different)
             df = pd.read_csv("notebook/data/stud.csv")
-
             logging.info("Read the dataset as dataframe")
 
-            os.makedirs(
-                os.path.dirname(self.ingestion_config.raw_data_path),
-                exist_ok=True
-            )
+            os.makedirs(os.path.dirname(self.ingestion_config.raw_data_path), exist_ok=True)
 
-            df.to_csv(
-                self.ingestion_config.raw_data_path,
-                index=False,
-                header=True
-            )
+            df.to_csv(self.ingestion_config.raw_data_path, index=False, header=True)
 
             logging.info("Train test split initiated")
+            train_set, test_set = train_test_split(df, test_size=0.2, random_state=42)
 
-            train_set, test_set = train_test_split(
-                df, test_size=0.2, random_state=42
-            )
-
-            train_set.to_csv(
-                self.ingestion_config.train_data_path,
-                index=False,
-                header=True
-            )
-
-            test_set.to_csv(
-                self.ingestion_config.test_data_path,
-                index=False,
-                header=True
-            )
+            train_set.to_csv(self.ingestion_config.train_data_path, index=False, header=True)
+            test_set.to_csv(self.ingestion_config.test_data_path, index=False, header=True)
 
             logging.info("Train test split completed")
 
@@ -69,8 +50,12 @@ class DataIngestion:
 
 if __name__ == "__main__":
     obj = DataIngestion()
-    train_data,test_data=obj.initiate_data_ingestion()
-    # obj.initiate_data_ingestion()
+    train_data, test_data = obj.initiate_data_ingestion()
 
     data_transformation = DataTransformation()
-    data_transformation.initiate_data_transformation(train_data,test_data)
+    train_arr, test_arr, _ = data_transformation.initiate_data_transformation(
+        train_data, test_data
+    )
+
+    model_trainer = ModelTrainer()
+    print(model_trainer.initiate_model_trainer(train_arr, test_arr))
